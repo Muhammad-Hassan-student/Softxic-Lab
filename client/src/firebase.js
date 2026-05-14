c2c809d3f1eefe684b57f8a998d203e06db93232
Muhammad-Hassan-student <muhammadhassanakram87@gmail.com>
2026-05-14 22:55:01 +0500

    change firebase oauth

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_KEY,
-  authDomain: "mern-blog-d815e.firebaseapp.com",
-  projectId: "mern-blog-d815e",
-  storageBucket: "mern-blog-d815e.appspot.com",
-  messagingSenderId: "309427200284",
-  appId: "1:309427200284:web:35d702d73e98ec8e1adad9"
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: "softxic-labs.firebaseapp.com",
+  projectId: "softxic-labs",
+  storageBucket: "softxic-labs.firebasestorage.app",
+  messagingSenderId: "280722817069",
+  appId: "1:280722817069:web:0d98c7f917cc217a0978a4",
+  measurementId: "G-3F7THEV0V6",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
